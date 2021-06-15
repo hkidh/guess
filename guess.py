52290f4f5bb1081119f60fd6dccc54bbf5835ac7
hkidh <hki.dh4@gmail.com>
2021-06-15 11:47:26 +0800

    final

--- guess.py
+++ guess.py
@@ -1,6 +1,15 @@
 import random
 y = 0
-r = random.randint(1,100)
+
+i = int(input('請輸入開始數字'))
+j = int(input('請輸入結束數字'))
+while i >= j:
+	print('結束不可小於開始,請重新輸入數字')
+	i = int(input('請輸入開始數字'))
+	j = int(input('請輸入結束數字'))
+	if i < j:
+		break
+r = random.randint(i,j)
 while True:
 	x = input('請輸入數字')
 	y = y + 1
