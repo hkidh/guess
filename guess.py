7e045708f559c29430d4d909e648c8423e1cdc3b
hkidh <hki.dh4@gmail.com>
2021-06-15 11:21:38 +0800

    how_many_guess

--- guess.py
+++ guess.py
@@ -1,11 +1,13 @@
 import random
-
+y = 0
 r = random.randint(1,100)
 while True:
 	x = input('請輸入數字')
+	y = y + 1
 	x = int(x)
 	if x == r:
 		print('你猜對了')
+		print('總共猜了', y, '次了')
 		break
 	elif x > r: 
 		print('請猜小點')
